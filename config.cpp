97a15acc61402cf1207ca5c34f56244d63b8e2ca
Bot <playerbotpro@hotmail.com>
2024-08-04 15:14:52 +0800

    class CfgVehicles

--- config.cpp
+++ config.cpp
@@ -6,3 +6,15 @@ class CfgPatches
     };
 };
 
+class CfgVehicles
+{
+	class Module_F;
+    class BOT_Module_LoadInventory: Module_F
+    {
+        scopeCurator=2;
+        category = "Curator";
+        displayName = "Set Unit Inventory Loadable";
+        function = "BOT_fnc_loadInventory";
+    };
+};
+
