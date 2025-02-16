4314bdb73af0cd51aea9c9739f8d37c7a73c85d5
Bot <playerbotpro@hotmail.com>
2025-02-16 23:23:34 +0800

    SCRIPTED WP: config

--- config.cpp
+++ config.cpp
@@ -21,3 +21,27 @@ class Extended_PostInit_EventHandlers
 		init="call compile preprocessFileLineNumbers 'BOT_Module_loadInventory\XEH_postInit.sqf'";
 	};
 };
+
+class cfgWaypoints {
+    class BOT {
+        displayName="BOT WPs";
+        class BOT_Rush {
+			displayName="BOT_Rush";
+			displayNameDebug="BOT_Rush";
+			tooltip="BOT_Rush with disableAI";
+			icon="\a3\3DEN\Data\CfgWaypoints\Move_ca.paa";
+			file="BOT_Module_loadInventory\fnc_wpRush.sqf";
+			class Attributes
+			{
+			};
+        };
+    };
+};
+
+class ZEN_WaypointTypes {
+    class BOT_Rush {
+        displayName = BOT_Rush;
+        type = "SCRIPTED";
+        script = "BOT_Module_loadInventory\fnc_wpRush.sqf";
+    };
+};
